6b7adc9b9cafc688246a95dec4bba06c55261eab
Mariana Barbosa <marianabarbosa@MacBook-Air-de-Mariana-4.local>
2022-10-19 12:47:04 +0100

    minor changes

--- 00_read_data.R
+++ 00_read_data.R
@@ -1,7 +1,7 @@
 
 # install.packages("data.table")
 library(data.table)
-path = "/Users/marianabarbosa/Documents/IMDB"
+path = "../IMBD-dataset-exploration-data"
 
 # missing values are denoted "\\N"
 
