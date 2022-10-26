42babf80196f5cd5c7fe006a830d4b50a8508ec9
Deborah Dore <deborah.dore@outlook.com>
2022-10-26 14:37:09 +0100

    final changes to hypothesis 3

--- 03_hypothesis_original_titles.R
+++ 03_hypothesis_original_titles.R
@@ -1,14 +1,15 @@
 library(data.table)
 library(ggplot2)
+theme_set(theme_bw())
 library(dplyr)
 library(cld3)
 library(cld2)
+library(viridis)
 
 path = "//Users/deborah/Documents/IMDB-dataset-exploration/dataset/"
+plot_path = "/Users/deborah/Documents/IMDB-dataset-exploration/plots/original_titles_hypothesis/"
 
 titles = fread(paste0(path, "/title.merged.csv"))
-#original = fread(paste0(path, "/title_original.csv"))
-
 
 original = titles[types == "original" & (
   titleType == "movie" |
@@ -34,6 +35,7 @@ gc()
 
 str(original)
 
+original = original[!duplicated(original[, tconst]), ]
 original[region == "", region := NA]
 original[language == "", language := NA]
 colnames(original)[2] = "title"
@@ -52,7 +54,7 @@ paste0("Percentage of NA values in region ",
 # the cld3 package is not able to guess all the languages, let's use also cld2
 original[is.na(language), language := cld2::detect_language(title)]
 
-# now we have only 15% of missing values
+# now we have only 16% of missing values
 paste0("Percentage of NA values in language ",
        as.integer(((count(
          original[is.na(language)]
@@ -63,300 +65,169 @@ nrow(original)
 original = na.omit(original, cols = "averageRating")
 original = na.omit(original, cols = "startYear")
 
-original = original[startYear >= 1900 & startYear <= 2022, ]
+original = original[startYear >= 1900 & startYear <= 2022,]
 
 
 ggplot(original, aes(x = startYear, fill = titleType)) +
-  ggtitle("Distribution of original titles during the years") +
   annotate(
     "rect",
-    fill = "yellow",
+    fill = "grey20",
     alpha = 0.4,
     xmin = 1929,
     xmax = 1945,
     ymin = -Inf,
     ymax = Inf
   ) +
+  scale_fill_viridis(discrete = TRUE, end = 0.8) +
+  facet_wrap( ~ titleType) +
   geom_bar()
+
+ggsave(
+  paste0(plot_path, "distribution_titles_per_titleType.pdf"),
+  width = 10,
+  height = 4
+)
 
 
 # Did the popularity of Italian/German/Japanese movie decreased/increased after
 # WW2? -------------------------------------------------------------------------
-it_original = original[language == 'it' | region == 'IT',]
-de_original = original[language == 'de' | region == 'DEU',]
-
-# ITALY ------------------------------------------------------------------------
-count(it_original[is.na(language),])
-
+it_original = original[language == 'it' | region == 'IT', ]
+de_original = original[language == 'de' | region == 'DEU', ]
+others_original = original[!(language %in% c("it", "de"))]
+others_original[, region := NULL]
+de_original[, region := NULL]
 it_original[, region := NULL]
 
+# ITALY ------------------------------------------------------------------------
+count(it_original[is.na(language), ])
+it_original = it_original[!(is.na(startYear))]
+
+# 13394
 paste0("Original italian movies: ", nrow(it_original))
 
 summary(it_original)
 
-# how many italian movies per year
-ggplot(it_original, aes(x = startYear, fill = titleType)) +
-  ggtitle("Distribution of italian titles during the years") +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1929,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_bar()
-
-
-# focus
-it_original[(startYear >= 1910 & startYear <= 1960),] %>%
-  ggplot(aes(x = startYear, fill = titleType)) +
-  ggtitle("Distribution of italian movies during fascism") +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1929,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_bar(position = "dodge")
-
 # GERMANY ----------------------------------------------------------------------
-count(de_original[is.na(language),])
-
-de_original[, region := NULL]
+count(de_original[is.na(language), ])
 de_original = de_original[!(is.na(startYear))]
 
-
+# 20728
 paste0("Original german movies: ", nrow(de_original))
 
 summary(de_original)
 
-ggplot(de_original, aes(x = startYear, fill = titleType)) +
-  ggtitle("Distribution of german titles during the years") +
+#-------------------------------------------------------------------------------
+df_original = rbindlist(list(it_original[, language := "italian"],
+                             de_original[, language := "german"],
+                             others_original[, language := "others"]))
+
+
+# distribution titles
+df_original[(startYear >= 1920 & startYear <= 1960), ] %>%
+  ggplot(aes(x = startYear, fill = titleType)) +
   annotate(
     "rect",
-    fill = "yellow",
+    fill = "grey20",
     alpha = 0.4,
     xmin = 1929,
     xmax = 1945,
     ymin = -Inf,
     ymax = Inf
   ) +
-  geom_bar()
+  facet_wrap(~ language, scales = "free") +
+  scale_fill_viridis(discrete = TRUE, end = 0.8) +
+  geom_histogram(binwidth = 1, alpha = 0.8)
 
+ggsave(
+  paste0(plot_path, "distribution_titles_per_language.pdf"),
+  width = 14,
+  height = 4
+)
 
-
-de_original[startYear >= 1910 & startYear <= 1960,] %>%
-  ggplot(aes(x = startYear, fill = titleType)) +
-  ggtitle("Distribution of german titles during nazism") +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1933,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_bar(position = "dodge")
 
 # POPULARITY -------------------------------------------------------------------
+
 it_mean = it_original %>% group_by(startYear, titleType) %>%
-  summarise(mean = mean(averageRating),
+  summarise(mean = weighted.mean(averageRating, numVotes),
             .groups = 'drop')
+
+it_mean$language = "italian"
 
 
 de_mean = de_original %>% group_by(startYear, titleType) %>%
-  summarise(mean = mean(averageRating),
+  summarise(mean = weighted.mean(averageRating, numVotes),
             .groups = 'drop')
 
+de_mean$language = "german"
 
-original_mean = original %>% group_by(startYear, titleType) %>%
-  summarise(mean = mean(averageRating),
+other_mean = others_original %>% group_by(startYear, titleType) %>%
+  summarise(mean = weighted.mean(averageRating, numVotes),
             .groups = 'drop')
 
-# POPULARITY OF ITALIAN MOVIES DURING THE YEARS
-ggplot() +
+other_mean$language = "others"
+
+# POPUlARITY OF TITLES DURING THE YEARS
+
+df = rbindlist(list(it_mean,de_mean,other_mean))
+
+df = df[titleType %in% c("movie", "short"),]
+
+# ggplot(df, aes(x = startYear, y = mean, color = titleType)) +
+#   annotate(
+#     "rect",
+#     fill = "grey20",
+#     alpha = 0.4,
+#     xmin = 1929,
+#     xmax = 1945,
+#     ymin = -Inf,
+#     ymax = Inf
+#   ) +
+#   facet_wrap(~ language) +
+#   geom_line()
+#
+# ggsave(
+#   paste0(
+#     plot_path,
+#     "popularity_of_movies_and_shorts_per_titleType.pdf"
+#   ),
+#   width = 10,
+#   height = 4
+# )
+
+# POPULARITY MOVIES/SHORT DURING 1920 - 1960
+ggplot(df,
+       aes(
+         x = startYear,
+         y = mean,
+         color = language,
+         size = language,
+         alpha = language
+       )) +
   annotate(
     "rect",
-    fill = "yellow",
+    fill = "grey20",
     alpha = 0.4,
     xmin = 1929,
     xmax = 1945,
     ymin = -Inf,
     ymax = Inf
   ) +
-  geom_line(data = it_mean %>% filter(titleType == "movie"),
-            aes(x = startYear, y = mean, colour = "ITALY")) +
-  geom_line(data = original_mean %>% filter(titleType == "movie"),
-            aes(x = startYear, y = mean, colour = "AVERAGE")) +
-  scale_color_manual(name = "nations",
-                     values = c("ITALY" = "darkblue", "AVERAGE" = "red")) +
-  ggtitle("Popularity of italian movies during the years")
+  facet_wrap( ~ titleType) +
+  scale_color_viridis(discrete = TRUE, end = 0.8) +
+  scale_color_manual(values = c("brown", "mediumpurple3", "black")) +
+  scale_size_manual(values = c(0.5, 0.5, 1)) +
+  scale_alpha_manual(values = c(1, 1, 0.8)) +
+  xlim(1920, 2010) +
+  geom_line()
 
-# POPULARITY OF GERMAN MOVIES DURING THE YEARS
-ggplot() +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1929,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +m
-  geom_line(data = de_mean %>% filter(titleType == "movie"),
-            aes(x = startYear, y = mean, colour = "GERMANY")) +
-  geom_line(data = original_mean %>% filter(titleType == "movie"),
-            aes(x = startYear, y = mean, colour = "AVERAGE")) +
-  scale_color_manual(name = "nations",
-                     values = c("GERMANY" = "darkblue", "AVERAGE" = "red")) +
-  ggtitle("Popularity of german movies during the years")
-
-
-# POPULARITY OF ITALIAN SHORT DURING THE YEARS
-ggplot() +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1929,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_line(data = it_mean %>% filter(titleType == "short"),
-            aes(x = startYear, y = mean, colour = "ITALY")) +
-  geom_line(data = original_mean %>% filter(titleType == "short"),
-            aes(x = startYear, y = mean, colour = "AVERAGE")) +
-  scale_color_manual(name = "nations",
-                     values = c("ITALY" = "darkblue", "AVERAGE" = "red")) +
-  ggtitle("Popularity of italian short during the years")
-
-# POPULARITY OF GERMAN SHORT DURING THE YEARS
-ggplot() +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1933,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_line(data = de_mean %>% filter(titleType == "short"),
-            aes(x = startYear, y = mean, colour = "GERMANY")) +
-  geom_line(data = original_mean %>% filter(titleType == "short"),
-            aes(x = startYear, y = mean, colour = "AVERAGE")) +
-  scale_color_manual(name = "nations",
-                     values = c("GERMANY" = "darkblue", "AVERAGE" = "red")) +
-  ggtitle("Popularity of german short during the years")
-
-# FOCUS OF ITALIAN MOVIES AND SHORT DURING FASCISM
-
-it_mean %>% filter(titleType == "movie" | titleType == "short") %>%
-  ggplot(aes(
-    x = startYear,
-    y = mean,
-    group = titleType,
-    color = titleType
-  )) +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1929,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  annotate(
-    "rect",
-    fill = "red",
-    alpha = 0.4,
-    xmin = 1939,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_line() +
-  ggtitle("Popularity of italian short and movies from 1900 to 1970") +
-  xlim(1900, 1970)
-
-# FOCUS OF GERMAN MOVIES AND SHORT DURING NAZISM
-de_mean %>% filter(titleType == "movie" | titleType == "short") %>%
-  ggplot(aes(
-    x = startYear,
-    y = mean,
-    group = titleType,
-    color = titleType
-  )) +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1933,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  annotate(
-    "rect",
-    fill = "red",
-    alpha = 0.4,
-    xmin = 1939,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_line() +
-  ggtitle("Popularity of german short and movies from 1900 to 1970") +
-  xlim(1900, 1970)
-
-# COMPARISON BETWEEN GERMAN AND ITALIAN MOVIES
-ggplot() +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1939,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_line(data = de_mean %>% filter(titleType == "movie"),
-            aes(x = startYear, y = mean, colour = "GERMANY")) +
-  geom_line(data = it_mean %>% filter(titleType == "movie"),
-            aes(x = startYear, y = mean, colour = "ITALY")) +
-  scale_color_manual(name = "nations",
-                     values = c("GERMANY" = "darkblue", "ITALY" = "red")) +
-  ggtitle("Comparison of opularity of german and italian movies") +
-  xlim(1920, 1960)
-
-
-# COMPARISON BETWEEN GERMAN AND ITALIAN SHORT
-ggplot() +
-  annotate(
-    "rect",
-    fill = "yellow",
-    alpha = 0.4,
-    xmin = 1939,
-    xmax = 1945,
-    ymin = -Inf,
-    ymax = Inf
-  ) +
-  geom_line(data = de_mean %>% filter(titleType == "short"),
-            aes(x = startYear, y = mean, colour = "GERMANY")) +
-  geom_line(data = it_mean %>% filter(titleType == "short"),
-            aes(x = startYear, y = mean, colour = "ITALY")) +
-  scale_color_manual(name = "nations",
-                     values = c("GERMANY" = "darkblue", "ITALY" = "red")) +
-  ggtitle("Comparison of opularity of german and italian short") +
-  xlim(1920, 1960)
+ggsave(
+  paste0(
+    plot_path,
+    "popularity_of_movies_and_shorts_per_titleType.pdf"
+  ),
+  width = 10,
+  height = 4
+)
 
 
 
