ef343dc9d52b9060a67a716f90481372d2554a61
schade-rebekka <woodstone@posteo.de>
2022-10-27 13:54:17 +0200

    update

---
+++ 03_eda_reb.R
@@ -0,0 +1,97 @@
+
+library(data.table)
+library(magrittr)
+library(tidyr)
+library(ggplot2); theme_set(theme_bw())
+library(viridis)
+library(scales)
+library(cowplot)
+options(scipen = 1000000)
+path = "../IMBD-dataset-exploration-data/"
+
+
+original = fread("../IMDB-dataset-exploration-data/title.original.csv")
+# original = fread(paste0(path, "title.original.csv"))
+nrow(original) - length(unique(original[, tconst]))  # 164 duplicate tconst
+original[, N := .N, by = tconst]
+original[N > 1, N := lapply(unique(tconst), function(t) {
+  dat = original[tconst == t]
+  # keep = dat[, title] == dat[, originalTitle]
+  # if (sum(keep) == 1) return(keep)
+  keep = !is.na(dat[, language]) | !is.na(dat[, region])
+  if (sum(keep) == 1) return(keep)
+  keep = !is.na(dat[, language])
+  if (sum(keep) == 1) return(keep)
+  else return(c(TRUE, rep(FALSE, nrow(dat) - 1)))
+}) %>% unlist()]
+original = original[N == 1]
+original[, N := NULL]
+nrow(original) - length(unique(original[, tconst]))  # now everything is all right
+
+original[, `:=`(tconst = NULL, title = NULL, isAdult = NULL, runtimeMinutes = NULL, language = NULL, region = NULL)]
+original = original[(!is.na(startYear)) & (startYear <= 2022) & (startYear >= 1897)]  # delete 1245621 rows (13.4%)
+original = original[genres != ""]  # delete 427915 rows (4.6%)
+original = original[titleType %in% c("movie", "short", "tvSeries", "tvMovie")]  # delete 7381423 rows (79.6% of which 75.4% tvEpisode)
+# original[, language := ifelse(language == "it", "Italian", ifelse(language == "de", "German", "Other"))]
+# original[, genres := strsplit(genres, "\\|")]
+gc()
+
+
+# development and distribution of titleTypes
+dt_titles = original
+dt_titles = dt_titles[, .N, by = .(startYear, titleType)]
+dt_titles[, share := N/sum(N), by = startYear]
+dt_titles[, Type := titleType]
+
+t_abs = ggplot(dt_titles, aes(x = startYear, y = N, fill = Type)) +
+  geom_area(position = "stack") +
+  scale_x_continuous(breaks = seq(1900, 2022, by = 25)) +
+  xlab("Start year") +
+  ylab("Number of titles") +
+  scale_fill_viridis(discrete = TRUE, end = 0.8) +
+  theme(legend.position = "none")
+  
+t_rel = ggplot(dt_titles, aes(x = startYear, y = share, fill = Type)) +
+  geom_area(position = "stack") +
+  scale_x_continuous(breaks = seq(1900, 2022, by = 25)) +
+  xlab("Start year") +
+  ylab("Distribution of titles") +
+  scale_fill_viridis(discrete = TRUE, end = 0.8) +
+  theme(legend.position = "none")
+
+# distribution averageRating
+rating = ggplot(original[!is.na(averageRating)], aes(x = averageRating, y = ..density..)) +
+  geom_histogram(bins = 30, fill = "royalblue4", color = "black") +
+  geom_density() +
+  xlab("Average rating of original titles") +
+  ylab("Density")
+
+# distribution numVotes
+votes = ggplot(original[!is.na(numVotes)], aes(x = numVotes, y = ..density..)) +
+  geom_histogram(bins = 30, fill = "royalblue4", color = "black") +
+  geom_density() +
+  scale_x_continuous(trans = "log10", breaks = c(0, 100, 1000, 10000, 100000)) +
+  xlab("Number of votes of original titles (log scale)") +
+  ylab("Density")
+
+# Connection averageRating and numVotes
+rat_vot = ggplot(original[!is.na(averageRating) & !is.na(numVotes)],
+       aes(x = numVotes, y = averageRating)) +
+  geom_hex(bins = 10) +
+  xlab("Number of votes of original titles\n(log scale)") +
+  ylab("Average rating of original titles") +
+  scale_fill_viridis(begin = 0.9, end = 0, trans = log10_trans(), option = "A") +
+  # scale_fill_continuous(low = viridis_pal(option = "F")(2)[[2]], high = viridis_pal(option = "F")(2)[[1]], trans = log10_trans()) +
+  scale_x_continuous(trans = "log10", breaks = c(0, 100, 1000, 10000, 100000))
+
+plot_grid(
+plot_grid(t_abs, t_rel,
+          get_legend(t_abs + guides(color = guide_legend(nrow = 1)) + theme(legend.position = "bottom")),
+          ncol = 1, rel_heights = c(1, 1, .2)),
+plot_grid(rating, votes, ncol = 1),
+plot_grid(ggplot() + theme(panel.border = element_blank()),
+          rat_vot,
+          ggplot() + theme(panel.border = element_blank()),
+          rel_heights = c(0.3, 1, 0.3), ncol = 1),
+ncol = 3)
+ggsave("plots/EDA.pdf", width = 12, height = 4)
--- 03_hypothesis_genres_reb.R
+++ 03_hypothesis_genres_reb.R
@@ -61,6 +61,15 @@ all_genres = c("History", "Drama", "Family", "Music",
                "Biography", "Comedy", "Action", "Talk-Show",
                "News", "Short", "Sci-Fi", "Film-Noir")
 
+all_genres = c("History", "Drama", "Family", "News",
+               "War", "Romance", "Animation", "Music",
+               "Documentary", "Crime", "Adventure", "Sport",
+               "Biography", "Comedy", "Action", "Talk-Show")#
+
+all_genres = c("History", "War", "Documentary", "Talk-Show",
+               "Drama", "Romance", "Crime", "Comedy",
+               "Family", "Animation", "Adventure", "Action")
+
 # create dataframe for plot
 tb_distr = original
 for (g_col in all_genres) {
@@ -83,7 +92,7 @@ tb_distr_text = unique(tb_distr_text)
 
 # plot with language
 ggplot(tb_distr_nocolor, aes(x = startYear, y = share, color = language, alpha = language, size = language)) +
-  geom_rect(xmin = 1929, xmax = 1945, ymin = 0, ymax = 1.2, alpha = 0.05, fill = "grey", color = "grey") +
+  geom_rect(xmin = 1929, xmax = 1945, ymin = 0, ymax = 1.2, alpha = 0.05, fill = "grey20", color = "grey") +
   geom_line(alpha = 0.8) +
   facet_wrap(~ genre, ncol = 4, scales = "free") +
   scale_x_continuous(breaks = seq(1900, 2022, by = 25)) +
@@ -108,6 +117,7 @@ tb_distr = melt(tb_distr, id.vars = c("startYear", "titleType"), variable.name =
 tb_distr[, total := sum(count), by = .(startYear)]
 tb_distr[, share := count/total]
 tb_distr[, `:=`(count = NULL, total = NULL)]
+tb_distr[, Type := titleType]
 
 tb_distr_nocolor = copy(tb_distr)
 tb_distr_nocolor[, titleType := NULL]
@@ -118,8 +128,8 @@ tb_distr_text[, startYear := NULL]
 tb_distr_text[, share := max(share), by = genre]
 tb_distr_text = unique(tb_distr_text)
 
-ggplot(tb_distr, aes(x = startYear, y = share, fill = titleType)) +
-  geom_rect(xmin = 1929, xmax = 1945, ymin = 0, ymax = 1, alpha = 0.8, fill = "grey") +
+ggplot(tb_distr, aes(x = startYear, y = share, fill = Type)) +
+  geom_rect(xmin = 1929, xmax = 1945, ymin = 0, ymax = 1, alpha = 0.8, fill = "grey60") +
   geom_area(position = "stack", alpha = 0.8) +
   facet_wrap(~ genre, ncol = 4, scales = "free") +
   scale_x_continuous(breaks = seq(1900, 2022, by = 25)) +
@@ -127,8 +137,8 @@ ggplot(tb_distr, aes(x = startYear, y = share, fill = titleType)) +
   xlab("Start year") +
   ylab("Share of each genre in all titles of one year") +
   geom_text(data = tb_distr_text, mapping = aes(x = 1937, y = share * 0.95, label = "1929 - 1945", fill = NULL), size = 3) +
-  theme(axis.title = element_text(size=14,face="bold"))
-ggsave("plots/03_hypothesis_genres_distribution_colored.jpg", width = 14, height = 10)
+  theme(axis.title = element_text(size=12,face="bold"))
+ggsave("plots/03_hypothesis_genres_distribution_colored.pdf", width = 10, height = 4.5)
 
 
 
@@ -171,4 +181,4 @@ ggplot(tb_war, aes(x = genre, y = share, fill = language, alpha = Period, label 
   theme(axis.title = element_text(size=12,face="bold"),
         axis.text.x = element_text(angle = 60, vjust = 1, hjust=1)) +
   facet_wrap(~ language, ncol = 3)
-ggsave("plots/03_hypothesis_genres_war_language.jpg", width = 12, height = 4)
+ggsave("plots/03_hypothesis_genres_war_language.pdf", width = 8, height = 2.5)
