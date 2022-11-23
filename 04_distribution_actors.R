65d0ceb3b30489f3b69a450ff52b119520ef2039
deborah dore <deborah.dore@outlook.com>
2022-11-23 12:53:15 +0000

    force network

--- 04_distribution_actors.R
+++ 04_distribution_actors.R
@@ -1,22 +1,24 @@
 library(data.table)
-library(dplyr)
 library(plyr)
+library(dplyr)
 library(d3Network)
+
 
 path = "//Users/deborah/Documents/IMDB-dataset-exploration/dataset/"
 
-principals = fread(paste0(path, "/title.principals_clean.csv"))
-crew = fread(paste0(path, "/title.crew_clean.csv"))
-
-series = fread(paste0(path, "/merged_series.csv"))
+series = fread(paste0(path, "/merged_series_withNA.csv"))
+series = series[success==TRUE]
 
 actorsList = series[, actors] %>% as.list()
 str(actorsList)
 
-nodes = data.table()
-nodes$actors = rapply(actorsList, function(x) strsplit(x, split = "[|]")) %>% 
-  as.integer() %>% unique() %>% as.data.frame()
+nodes = data.frame()
+l = rapply(actorsList, function(x) strsplit(x, split = "[|]")) %>% 
+  as.integer() %>% unique()
+nodes<- data.frame(matrix(unlist(l), nrow=length(l), byrow=TRUE))
+colnames(nodes) <- c('name')
 nodes$group = 1
+nodes = na.omit(nodes)
 
 source = c()
 target = c()
@@ -27,17 +29,26 @@ for (x in actorsList) {
   if(length(elem) > 1){
     for (y in elem){
       for(k in elem){
-        source <- append(source,y)
-        target <- append(target, k)
+        if(k!=y){
+          source <- append(source,y)
+          target <- append(target, k)
+        }
       }
     }
   }
 }
 
-links = data.frame(source, target)
-links$value = 1
-links %>% group_by(source, target) %>% summarize(x = sum(value))
+links = data.table(source, target)
+links[, value:=1]
+links_new = links[, value := sum(value), by = .(source, target)]
+links_new = unique(links_new)
 
-d3ForceNetwork(Links = links, Nodes = nodes,
-               Source = "source", Target = "target",
-               NodeID = "actors", value=value, opacity = 0.8)
+
+
+sink("test.html")
+d3ForceNetwork(Links = as.data.frame(links_new), 
+               Nodes = nodes, 
+               Source = "source",
+               Target = "target", NodeID = "name",
+               Value = "value",
+               Group = "group", opacity = 0.4)
